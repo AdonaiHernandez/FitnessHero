a7b9cf35d9ac8a20ff5d11cea3d1a9ebd02cf641
Jmdh03 <josediher2003@gmail.com>
2025-04-18 13:28:19 +0100

    cerrar sesión

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 
 export default function ProfileScreen() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('isLoggedIn');
+    Alert.alert('Has cerrado sesión');
+    router.push('/login');
+  };
+
   return (
     <View style={styles.container}>
       <Header />
@@ -13,6 +24,11 @@ export default function ProfileScreen() {
           <Text style={styles.infoText}>Nivel: 5</Text>
           <Text style={styles.infoText}>Pasos totales: 85,000</Text>
         </View>
+
+        {/* Botón para cerrar sesión */}
+        <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
+          <Text style={styles.logoutButtonText}>Cerrar sesión</Text>
+        </TouchableOpacity>
       </View>
       <Footer />
     </View>
@@ -21,8 +37,37 @@ export default function ProfileScreen() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#4B0082' },
-  mainContent: { flex: 1, padding: 20, backgroundColor: '#E6FFE6', justifyContent: 'center' },
-  title: { fontSize: 24, fontWeight: 'bold', color: '#4B0082', textAlign: 'center', marginBottom: 20 },
-  infoBox: { backgroundColor: '#6A0DAD', borderRadius: 10, padding: 20 },
+  mainContent: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#E6FFE6',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#4B0082',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  infoBox: {
+    backgroundColor: '#6A0DAD',
+    borderRadius: 10,
+    padding: 20,
+    width: '100%',
+    marginBottom: 30,
+  },
   infoText: { color: '#FFF', fontSize: 16, marginBottom: 10 },
+  logoutButton: {
+    backgroundColor: '#4B0082',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+  },
+  logoutButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
