6fd201d4eb2bd07daf8d40052a7f1c44935c2f12
Adonai Hernandez Bolaños <adonai.acsii@gmail.com>
2025-04-22 12:13:43 +0100

    Renderizado el personaje con texturas y animaciones

--- app/(tabs)/avatar.tsx
+++ app/(tabs)/avatar.tsx
@@ -1,11 +1,19 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Viewer from '@/components/Viewer';
 import Footer from '@/components/footer';
+import Header from '@/components/header';
 
-export default function StoreScreen() {
+
+export default function avatar() {
   return (
     <View style={styles.container}>
-      <Viewer/>
+      <Header />
+            <View style={styles.mainContent}>
+              <Text style={styles.title}>Avatar</Text>
+              <Viewer/>
+            </View>
+            <Footer />
+      
     </View>
   );
 }
@@ -15,5 +23,5 @@ const styles = StyleSheet.create({
   mainContent: { flex: 1, padding: 20, backgroundColor: '#E6FFE6' },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, color: '#4B0082', textAlign: 'center' },
   item: { backgroundColor: '#6A0DAD', padding: 15, borderRadius: 10, marginVertical: 10 },
-  itemText: { color: '#FFF', fontWeight: 'bold', fontSize: 16, textAlign: 'center' },
+  itemText: { color: '#FFF', fontWeight: 'bold', fontSize: 16, textAlign: 'center' }
 });
